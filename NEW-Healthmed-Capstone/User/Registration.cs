f382edfed6140a3b5a3cb3d39b4d2570918f5f35
Joe Marie Aquino <aquino.092130@tarlac.sti.edu.ph>
2023-01-19 23:05:47 +0800

    d

--- NEW-Healthmed-Capstone/User/Registration.cs
+++ NEW-Healthmed-Capstone/User/Registration.cs
@@ -1,14 +1,6 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace NEW_Healthmed_Capstone.User

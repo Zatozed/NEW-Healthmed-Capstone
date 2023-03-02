a3ae422e7221ea57b058c71e506b4b39a4a861ab
Joe Marie Aquino <aquino.092130@tarlac.sti.edu.ph>
2023-03-03 00:46:39 +0800

    compute in cashreg rpt

--- NEW-Healthmed-Capstone/Reports/CashRegReportGenerator.cs
+++ NEW-Healthmed-Capstone/Reports/CashRegReportGenerator.cs
@@ -9,6 +9,10 @@ namespace NEW_Healthmed_Capstone.Reports
     {
         DBhelperClass dbh = new DBhelperClass();
 
+        double cash_out_total;
+        double starting;
+        double ending;
+        double ending_plus_cash_out;
         public CashRegReportGenerator()
         {
             InitializeComponent();
@@ -27,15 +31,54 @@ namespace NEW_Healthmed_Capstone.Reports
 
             dgvCashReg.AutoGenerateColumns = false;
         }
+        private void GetEdCash()
+        {
+            ending = 0;
+
+            if (dgvCashReg.Rows != null)
+            {
+                foreach (DataGridViewRow r in dgvCashReg.Rows)
+                {
+                    if (r.Cells["colAction"].Value.ToString().Equals("Ending"))
+                    {
+                        ending = Convert.ToDouble(r.Cells["colAmount"].Value);
+                    }
+                }
+            }
+        }
+
+        private void ComputeEndingPlusCashOut()
+        {
+            ending_plus_cash_out = ending + cash_out_total;
+        }
+
+        private void ComputeTotalCashOut()
+        {
+            cash_out_total = 0;
+
+            if (dgvCashReg.Rows != null)
+            {
+                foreach (DataGridViewRow r in dgvCashReg.Rows)
+                {
+                    if (r.Cells["colAction"].Value.ToString().Equals("Cash Out"))
+                    {
+                        cash_out_total += Convert.ToDouble(r.Cells["colAmount"].Value);
+                    }
+                }
+            }
+            MessageBox.Show(cash_out_total.ToString());
+        }
 
         private void dt1_ValueChanged(object sender, EventArgs e)
         {
             dgvCashReg.DataSource = dbh.ShowCashReg(cbUsers.Text, dt1.Value.ToString("yyyy-MM-dd"));
+            ComputeTotalCashOut();
         }
 
         private void cbUsers_SelectedIndexChanged(object sender, EventArgs e)
         {
             dgvCashReg.DataSource = dbh.ShowCashReg(cbUsers.Text, dt1.Value.ToString("yyyy-MM-dd"));
+            ComputeTotalCashOut();
         }
     }
 }

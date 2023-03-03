90e1314b6b1ea3c309514a049d3f53cf3a641829
Joe Marie Aquino <aquino.092130@tarlac.sti.edu.ph>
2023-03-03 20:26:28 +0800

    stock adjustment updating problem

--- NEW-Healthmed-Capstone/Inv/StockAdjustment.cs
+++ NEW-Healthmed-Capstone/Inv/StockAdjustment.cs
@@ -47,126 +47,52 @@ namespace NEW_Healthmed_Capstone.Inv
 
         private void DGVStockUdjustment_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
+            double UnitCost = 0;
+            double UnitPrice = 0;
+            int inStockQTY = 0;
+
             if (DGVStockUdjustment.Columns[e.ColumnIndex].Name == "colUpdate")
             {
                 try
                 {
-                    double UnitCost = Convert.ToDouble(DGVStockUdjustment.Rows[e.RowIndex].Cells["colNewUnitCost"].Value);
-                    double UnitPrice = Convert.ToDouble(DGVStockUdjustment.Rows[e.RowIndex].Cells["colNewUnitPrice"].Value);
-                    int inStockQTY = Convert.ToInt32(DGVStockUdjustment.Rows[e.RowIndex].Cells["colNewInStock"].Value);
+                    if (string.IsNullOrEmpty(Convert.ToString(DGVStockUdjustment.Rows[e.RowIndex].Cells["colNewUnitCost"].Value)) == false)
+                        UnitCost = Convert.ToDouble(DGVStockUdjustment.Rows[e.RowIndex].Cells["colNewUnitCost"].Value);
+
+                    if (string.IsNullOrEmpty(Convert.ToString(DGVStockUdjustment.Rows[e.RowIndex].Cells["colNewUnitPrice"].Value)) == false)
+                        UnitPrice = Convert.ToDouble(DGVStockUdjustment.Rows[e.RowIndex].Cells["colNewUnitPrice"].Value);
+
+                    if (string.IsNullOrEmpty(Convert.ToString(DGVStockUdjustment.Rows[e.RowIndex].Cells["colNewInStock"].Value)) == false)
+                        inStockQTY = Convert.ToInt32(DGVStockUdjustment.Rows[e.RowIndex].Cells["colNewInStock"].Value);
 
                     if (UnitCost < 0 || UnitPrice < 0 || inStockQTY < 0)
                     {
                         MessageBox.Show("Invalid Input can't add a negative Value");
                     }
-                    else { 
-                    //====================================Update Unit Cost=====================================
-                    if (string.IsNullOrEmpty(Convert.ToString(DGVStockUdjustment.Rows[e.RowIndex].Cells["colNewUnitCost"].Value)) == false)
-                    {
-                        double convert = Convert.ToDouble(DGVStockUdjustment.Rows[e.RowIndex].Cells["colNewUnitCost"].Value);
-                        if (convert < 0)
+                    else
+                    { 
+                        //====================================Update Unit Cost=====================================
+                        if (string.IsNullOrEmpty(Convert.ToString(DGVStockUdjustment.Rows[e.RowIndex].Cells["colNewUnitCost"].Value)) == false)
                         {
-                            MessageBox.Show("Invalid Input");
-                        }
-                        else
-                        {
-                            dbh.CloseCon();
-                            dbh.OpenCon();
-                            cmd = new MySqlCommand("UPDATE tbl_products SET unit_cost = @unitCost WHERE p_id = @ID", dbh.conn());
-                            cmd.Parameters.AddWithValue("@unitCost", Convert.ToString(DGVStockUdjustment.Rows[e.RowIndex].Cells["colNewUnitCost"].Value));
-                            cmd.Parameters.AddWithValue("@ID", Convert.ToString(DGVStockUdjustment.Rows[e.RowIndex].Cells["productID"].Value));
-                            cmd.ExecuteNonQuery();
-                            if (cmd.ExecuteNonQuery() == 1)
+                            double convert = Convert.ToDouble(DGVStockUdjustment.Rows[e.RowIndex].Cells["colNewUnitCost"].Value);
+                            if (convert < 0)
                             {
-                                MessageBox.Show("Unit Cost Updated");
-                                dbh.CloseCon();
-
-                                DGVStockUdjustment.Rows[e.RowIndex].Cells["colCost"].Value = convert.ToString("0.00");
-                                DGVStockUdjustment.Rows[e.RowIndex].Cells["colNewUnitCost"].Value = "";
+                                MessageBox.Show("Invalid Input");
                             }
                             else
                             {
-                                MessageBox.Show("Not Updated");
-                            }
-                        }
-                    }
-                    //==========================================Update Unit Price=====================================
-                    if (string.IsNullOrEmpty(Convert.ToString(DGVStockUdjustment.Rows[e.RowIndex].Cells["colNewUnitPrice"].Value)) == false)
-                    {
-                        double convert = Convert.ToDouble(DGVStockUdjustment.Rows[e.RowIndex].Cells["colNewUnitPrice"].Value);
-                        if (convert < 0)
-                        {
-                            MessageBox.Show("Invalid Input");
-                        }
-                        else
-                        {
-                            dbh.CloseCon();
-                            dbh.OpenCon();
-                            cmd = new MySqlCommand("UPDATE tbl_products SET unit_price = @unitPrice WHERE p_id = @ID", dbh.conn());
-                            cmd.Parameters.AddWithValue("@unitPrice", Convert.ToString(DGVStockUdjustment.Rows[e.RowIndex].Cells["colNewUnitPrice"].Value));
-                            cmd.Parameters.AddWithValue("@ID", Convert.ToString(DGVStockUdjustment.Rows[e.RowIndex].Cells["productID"].Value));
-                            cmd.ExecuteNonQuery();
-                            if (cmd.ExecuteNonQuery() == 1)
-                            {
-                                MessageBox.Show("Unit Price Updated");
-                                dbh.CloseCon();
-
-                                DGVStockUdjustment.Rows[e.RowIndex].Cells["colPrice"].Value = convert.ToString("0.00");
-                                DGVStockUdjustment.Rows[e.RowIndex].Cells["colNewUnitPrice"].Value = "";
-                            }
-                            else
-                            {
-                                MessageBox.Show("Not Updated");
-                            }
-                        }
-                    }
-                    //========================================Update In Stock QTY===============================================
-                    if (string.IsNullOrEmpty(Convert.ToString(DGVStockUdjustment.Rows[e.RowIndex].Cells["colNewInStock"].Value)) == false)
-                    {
-                        int convert = Convert.ToInt32(DGVStockUdjustment.Rows[e.RowIndex].Cells["colNewInStock"].Value);
-                        if (convert < 0)
-                        {
-                            MessageBox.Show("Invalid Input");
-                        }
-                        else
-                        {
-                            dbh.CloseCon();
-                            dbh.OpenCon();
-                            cmd = new MySqlCommand("UPDATE tbl_products SET in_stock_qty = @instock WHERE p_id = @ID", dbh.conn());
-                            cmd.Parameters.AddWithValue("@instock", Convert.ToString(DGVStockUdjustment.Rows[e.RowIndex].Cells["colNewInStock"].Value));
-                            cmd.Parameters.AddWithValue("@ID", Convert.ToString(DGVStockUdjustment.Rows[e.RowIndex].Cells["productID"].Value));
-                            cmd.ExecuteNonQuery();
-                            if (cmd.ExecuteNonQuery() == 1)
-                            {
-                                MessageBox.Show("In Stock Qty. Updated");
-                                dbh.CloseCon();
-
-                                DGVStockUdjustment.Rows[e.RowIndex].Cells["colInStock"].Value = convert.ToString();
-                                DGVStockUdjustment.Rows[e.RowIndex].Cells["colNewInStock"].Value = "";
-                            }
-                            else
-                            {
-                                MessageBox.Show("Not Updated");
-                            }
-                        }
-                    }
-                        //==========================================Remarks Update===============================================
-
-                        if (string.IsNullOrEmpty(Convert.ToString(DGVStockUdjustment.Rows[e.RowIndex].Cells["colNewRemarks"].Value)) == false)
-                        {
                                 dbh.CloseCon();
                                 dbh.OpenCon();
-                                cmd = new MySqlCommand("UPDATE tbl_products SET remarks = @remarks WHERE p_id = @ID", dbh.conn());
-                                cmd.Parameters.AddWithValue("@remarks", Convert.ToString(DGVStockUdjustment.Rows[e.RowIndex].Cells["colNewRemarks"].Value));
+                                cmd = new MySqlCommand("UPDATE tbl_products SET unit_cost = @unitCost WHERE p_id = @ID", dbh.conn());
+                                cmd.Parameters.AddWithValue("@unitCost", Convert.ToString(DGVStockUdjustment.Rows[e.RowIndex].Cells["colNewUnitCost"].Value));
                                 cmd.Parameters.AddWithValue("@ID", Convert.ToString(DGVStockUdjustment.Rows[e.RowIndex].Cells["productID"].Value));
                                 cmd.ExecuteNonQuery();
                                 if (cmd.ExecuteNonQuery() == 1)
                                 {
-                                    MessageBox.Show("Remarks Updated");
+                                    MessageBox.Show("Unit Cost Updated");
                                     dbh.CloseCon();
 
-                                DGVStockUdjustment.Rows[e.RowIndex].Cells["colRemark"].Value = DGVStockUdjustment.Rows[e.RowIndex].Cells["colNewRemarks"].Value.ToString();
-                                    DGVStockUdjustment.Rows[e.RowIndex].Cells["colNewRemarks"].Value = "";
+                                    DGVStockUdjustment.Rows[e.RowIndex].Cells["colCost"].Value = convert.ToString("0.00");
+                                    DGVStockUdjustment.Rows[e.RowIndex].Cells["colNewUnitCost"].Value = "";
                                 }
                                 else
                                 {
@@ -174,7 +100,91 @@ namespace NEW_Healthmed_Capstone.Inv
                                 }
                             }
                         }
+                        //==========================================Update Unit Price=====================================
+                        if (string.IsNullOrEmpty(Convert.ToString(DGVStockUdjustment.Rows[e.RowIndex].Cells["colNewUnitPrice"].Value)) == false)
+                        {
+                            double convert = Convert.ToDouble(DGVStockUdjustment.Rows[e.RowIndex].Cells["colNewUnitPrice"].Value);
+                            if (convert < 0)
+                            {
+                                MessageBox.Show("Invalid Input");
+                            }
+                            else
+                            {
+                                dbh.CloseCon();
+                                dbh.OpenCon();
+                                cmd = new MySqlCommand("UPDATE tbl_products SET unit_price = @unitPrice WHERE p_id = @ID", dbh.conn());
+                                cmd.Parameters.AddWithValue("@unitPrice", Convert.ToString(DGVStockUdjustment.Rows[e.RowIndex].Cells["colNewUnitPrice"].Value));
+                                cmd.Parameters.AddWithValue("@ID", Convert.ToString(DGVStockUdjustment.Rows[e.RowIndex].Cells["productID"].Value));
+                                cmd.ExecuteNonQuery();
+                                if (cmd.ExecuteNonQuery() == 1)
+                                {
+                                    MessageBox.Show("Unit Price Updated");
+                                    dbh.CloseCon();
+
+                                    DGVStockUdjustment.Rows[e.RowIndex].Cells["colPrice"].Value = convert.ToString("0.00");
+                                    DGVStockUdjustment.Rows[e.RowIndex].Cells["colNewUnitPrice"].Value = "";
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Not Updated");
+                                }
+                            }
+                        }
+                        //========================================Update In Stock QTY===============================================
+                        if (string.IsNullOrEmpty(Convert.ToString(DGVStockUdjustment.Rows[e.RowIndex].Cells["colNewInStock"].Value)) == false)
+                        {
+                            int convert = Convert.ToInt32(DGVStockUdjustment.Rows[e.RowIndex].Cells["colNewInStock"].Value);
+                            if (convert < 0)
+                            {
+                                MessageBox.Show("Invalid Input");
+                            }
+                            else
+                            {
+                                dbh.CloseCon();
+                                dbh.OpenCon();
+                                cmd = new MySqlCommand("UPDATE tbl_products SET in_stock_qty = @instock WHERE p_id = @ID", dbh.conn());
+                                cmd.Parameters.AddWithValue("@instock", Convert.ToString(DGVStockUdjustment.Rows[e.RowIndex].Cells["colNewInStock"].Value));
+                                cmd.Parameters.AddWithValue("@ID", Convert.ToString(DGVStockUdjustment.Rows[e.RowIndex].Cells["productID"].Value));
+                                cmd.ExecuteNonQuery();
+                                if (cmd.ExecuteNonQuery() == 1)
+                                {
+                                    MessageBox.Show("In Stock Qty. Updated");
+                                    dbh.CloseCon();
+
+                                    DGVStockUdjustment.Rows[e.RowIndex].Cells["colInStock"].Value = convert.ToString();
+                                    DGVStockUdjustment.Rows[e.RowIndex].Cells["colNewInStock"].Value = "";
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Not Updated");
+                                }
+                            }
+                        }
+                        //==========================================Remarks Update===============================================
+
+                        if (string.IsNullOrEmpty(Convert.ToString(DGVStockUdjustment.Rows[e.RowIndex].Cells["colNewRemarks"].Value)) == false)
+                        {
+                            dbh.CloseCon();
+                            dbh.OpenCon();
+                            cmd = new MySqlCommand("UPDATE tbl_products SET remarks = @remarks WHERE p_id = @ID", dbh.conn());
+                            cmd.Parameters.AddWithValue("@remarks", Convert.ToString(DGVStockUdjustment.Rows[e.RowIndex].Cells["colNewRemarks"].Value));
+                            cmd.Parameters.AddWithValue("@ID", Convert.ToString(DGVStockUdjustment.Rows[e.RowIndex].Cells["productID"].Value));
+                            cmd.ExecuteNonQuery();
+                            if (cmd.ExecuteNonQuery() == 1)
+                            {
+                                MessageBox.Show("Remarks Updated");
+                                dbh.CloseCon();
+
+                                DGVStockUdjustment.Rows[e.RowIndex].Cells["colRemark"].Value = DGVStockUdjustment.Rows[e.RowIndex].Cells["colNewRemarks"].Value.ToString();
+                                    DGVStockUdjustment.Rows[e.RowIndex].Cells["colNewRemarks"].Value = "";
+                             }
+                             else
+                             {
+                                    MessageBox.Show("Not Updated");
+                             }
+                        }
                     }
+                }
                 
                 catch (FormatException fe)
                 {
